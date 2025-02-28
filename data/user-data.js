bd8f3823996dbcf4508ccdfe78103df102b51f9d
AnhDung-p <anhdungxyz2020@gmail.com>
2025-02-28 17:25:17 +0700

    commit

--- data/user-data.js
+++ data/user-data.js
@@ -1,8 +1,8 @@
 export const userData = {
-  githubUser: 'TakunKenjo',
-  devUsername: "TakunKenjo",
-  github: 'https://github.com/TakunKenjo',
-  facebook: 'https://www.facebook.com/trongduc.04',
-  skills: ['Boostrap','Java','Python','MySQL'],
+  githubUser: 'AnhDung-p',
+  devUsername: "AnhDung-p",
+  github: 'https://github.com/AnhDung-p',
+  facebook: 'https://www.facebook.com/dung.phananh.399/?_rdc=1&_rdr#',
+  skills: ['c++','Java','Python','MySQL'],
   timezone: '+7'
 };
